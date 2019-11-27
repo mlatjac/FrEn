6682c5af407fb133221decbe3d554fba4b062260
mlatjac <mlatjac@miguellegault.org>
2019-11-27 21:50:08 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/Language.php
+++ src/Http/Middleware/Language.php
@@ -3,11 +3,7 @@
 namespace Mlatjac\FrEn\Http\Middleware;
 
 use Closure;
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Session;
 
 class Language
